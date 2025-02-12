f51d706f3ab0df4592b27adbf13e785091313709
Cleitonlima810 <45742145+Cleitonlima810@users.noreply.github.com>
2025-02-11 21:09:35 -0300

    Update script.js
    Solução que encontrei, como solicitado na aula.

--- src/script.js
+++ src/script.js
@@ -1,7 +1,30 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//ESCREVA SEU CÓDIGO AQUI!!
+function Calculadora(num1, num2, operacao) {
+  let numero1 = parseFloat(num1);
+  let numero2 = parseFloat(num2);
+
+  switch (operacao) {
+    case '+':
+        return numero1 + numero2;
+    case '-':
+        return numero1 - numero2;
+    case '*':
+        return numero1 * numero2;
+    case '/':
+      return numero2 !== 0 ? numero1 / numero2 : "Erro: Divisão por zero";
+    case 'e': // Exponenciação manual com loop
+      let resultado = 1;
+      for (let i = 0; i < numero2; i++) {
+        resultado *= numero1;
+      }
+      return resultado;
+    default:
+      return "Erro: Operador inválido.";
+  }
+
+}
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
